85c6e2728a3ac68c53a4a2d2defeb30e0931d3c5
apurva112 <e19cse203@bennett.edu.in>
2020-09-16 11:28:13 +0530

    minor changes

--- lib/pages/onBoarding.dart
+++ lib/pages/onBoarding.dart
@@ -272,7 +272,7 @@ class _LoginPageState extends State<LoginPage> {
                       hint: "Enter Email...",
                     ),
                     SizedBox(height: 20,),
-                    InputWithIcon(
+                    PassWithIcon(
                       icon: Icons.vpn_key,
                       hint: "Enter Password...",
                     )
@@ -345,7 +345,7 @@ class _LoginPageState extends State<LoginPage> {
                       hint: "Enter Email...",
                     ),
                     SizedBox(height: 20,),
-                    InputWithIcon(
+                    PassWithIcon(
                       icon: Icons.vpn_key,
                       hint: "Enter Password...",
                     )
@@ -417,6 +417,53 @@ class _InputWithIconState extends State<InputWithIcon> {
                   border: InputBorder.none,
                   hintText: widget.hint
               ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PassWithIcon extends StatefulWidget {
+  final IconData icon;
+  final String hint;
+  PassWithIcon({this.icon, this.hint});
+
+  @override
+  _PassWithIconState createState() => _PassWithIconState();
+}
+
+class _PassWithIconState extends State<PassWithIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+              color: Color(0xFFB056674),
+              width: 2
+          ),
+          borderRadius: BorderRadius.circular(50)
+      ),
+      child: Row(
+        children: <Widget>[
+          Container(
+              width: 60,
+              child: Icon(
+                widget.icon,
+                size: 20,
+                color: Color(0xFFB056674),
+              )
+          ),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 20),
+                  border: InputBorder.none,
+                  hintText: widget.hint
+              ),
+              autofocus: false,
+              obscureText: true,
             ),
           )
         ],
